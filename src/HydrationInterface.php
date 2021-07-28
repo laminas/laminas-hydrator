0cac8d6306f096f9ddd1bf0d0f74c3e9746dd2f6
sasezaki <sasezaki@gmail.com>
2021-07-28 10:24:08 +0900

    adding template annotate
    Signed-off-by: sasezaki <sasezaki@gmail.com>

--- src/HydrationInterface.php
+++ src/HydrationInterface.php
@@ -13,6 +13,9 @@ interface HydrationInterface
      * @return object The implementation should return an object of any type.
      *     By purposely omitting the return type from the signature,
      *     implementations may choose to specify a more specific type.
+     * @psalm-param T $object
+     * @psalm-return T
+     * @template T of object
      */
     public function hydrate(array $data, object $object);
 }
