4b95e81c84b24feafe57d62bb4cdbd14d7aa9ebd
George Steel <george@net-glue.co.uk>
2022-12-13 16:23:40 +0000

    Added missing templates to Hydrate and Extract events marking them final
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- src/Aggregate/ExtractEvent.php
+++ src/Aggregate/ExtractEvent.php
@@ -11,6 +11,10 @@ use function array_merge;
 /**
  * Event triggered when the {@see AggregateHydrator} extracts
  * data from an object
+ *
+ * @template TTarget of object
+ * @extends Event<TTarget, array<empty, empty>>
+ * @final
  */
 class ExtractEvent extends Event
 {
@@ -24,10 +28,10 @@ class ExtractEvent extends Event
     /** @var mixed[] Data being extracted from the $extractionObject */
     protected $extractedData = [];
 
+    /** @psalm-param TTarget $target */
     public function __construct(object $target, protected object $extractionObject)
     {
-        parent::__construct();
-        $this->target = $target;
+        parent::__construct(self::EVENT_EXTRACT, $target, []);
     }
 
     /**
